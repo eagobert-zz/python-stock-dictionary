75c94cc9175d435ee79c72f34dba83fe8d95dcc3
eagobert <eagobert@hotmail.com>
2018-07-10 11:05:29 -0500

    update with steve solution

--- stocks.py
+++ stocks.py
@@ -11,7 +11,8 @@ print(stockDict)
 #Step 1): Create a list of tuples.  Each tuple is a stock purchase
 stockPurch = [( 'AAPL', 1000, '10-sep-2018', 100 ), ( 'EA', 1000, '1-apr-2018', 50 ), ( 'WMT', 1000, '1-jul-2018', 25 ),( 'AAPL', 2000, '20-sep-2018', 90 ), ( 'EA', 2000, '20-apr-2018', 45 ), ( 'WMT', 2000, '10-jul-2018', 20 )]
 
-#Step 2): Loop through list of tuples and find the name of the associated stock ticker 
+#Step 2): Loop through list of tuples and find the name of the associated stock ticker
+# Can also do this with a try/catch and an append.... 
 for purchase in stockPurch:
     # print ('Stock Purchase =', purchase)
     print ('Company Name', stockDict[purchase[0]])
@@ -30,9 +31,13 @@ for purchase in stockPurch:
 
     #if ticker exists in purchase_summary dictionary
     if purchase[0] in purchase_summary:
-
+        ticker = purchase[0]
+        indiv_purch = [(purchase[1], purchase[2], purchase[3])]
         #append the purchase to the ticker key
-        purchase_summary[purchase[0]] += [(purchase[1], purchase[2], purchase[3])]
+        # purchase_summary[purchase[0]] += [(purchase[1], purchase[2], purchase[3])]
+
+        #this also works
+        purchase_summary[ticker].append(indiv_purch)
 
     #if ticker key doesn't exist,
     else:
@@ -40,3 +45,40 @@ for purchase in stockPurch:
         purchase_summary[purchase[0]] = [(purchase[1], purchase[2], purchase[3])]
 
 print(purchase_summary)
+
+#Steve's solution:
+# portfolio = dict()
+# for purchase in purchases:
+#     ticker = purchase[0]
+
+#     full_company_name = stockDict[ticker]
+#     number_of_shares = purchase[1]
+#     purchase_price = purchase[3]
+#     full_purchase_price = number_of_shares * purchase_price
+
+#     minimal_purchase = (purchase[1], purchase[2], purchase[3])
+
+#     try:
+#         portfolio[ticker].append(purchase) # Append the purchase to the ticker list
+#     except KeyError:
+#         portfolio[ticker] = list() # If the key doesn't exist yet, create it
+#         portfolio[ticker].append(purchase)
+
+
+#     print("I purchased {} stock for ${}".format(full_company_name, full_purchase_price))
+
+# print(portfolio)
+
+
+
+
+
+
+
+# for ticker,purchases in portfolio.items():
+#     print("------ {} ------".format(ticker))
+#     total_portfolio_stock_value = 0
+#     for purchase in purchases:
+#         total_portfolio_stock_value += purchase[1] * purchase[3]
+#         print("    {}".format(purchase))
+#     print("Total value of stock in portfolio: ${}\n\n".format(total_portfolio_stock_value))
